96b04f371ba2f1ad3e8ab21b49905c99b04d91ae
Shami_Mourya <shamimourya9852@gmail.com>
2023-06-18 19:18:36 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { fakeFetch } from "./fakeFetch";
+
+import Homepage from './homepage';
+import AddHabit from './addHabit';
 import './App.css';
 
 function App() {
+  const [habits, setHabits] = useState([]);
+  const getData = async () => {
+    try {
+      const { status, data } = await fakeFetch("https://example.com/api/habits");
+      if (status === 200) {
+        setHabits(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={
+          <Homepage habits = {habits} />
+          }></Route>
+          <Route path='/addHabit' element={<AddHabit />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
