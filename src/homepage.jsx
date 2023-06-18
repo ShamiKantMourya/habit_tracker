a130f2113cde2171804180d64e2a9adaaf4efdf2
Shami_Mourya <shamimourya9852@gmail.com>
2023-06-18 19:42:59 +0530

    hompage.jsx

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import "./addHabits.css";
+
 const Homepage = (props) => {
     const {habits} = props;
     // console.log(habits)
   return (
-    <div>
-        <Link to="/addHabit" className='text'>Create your own habit</Link>
+    <div className='main'>
         <div className='defaulttask'>
+        <Link to="/addHabit" className='text'>Create your own habit</Link>
         {
             habits.map(({id, image, task}) => 
             <div className='task-card'>
                 <div className='task' key={id}>
                     <img src= {image} alt={task} />
                     <h3>{task}</h3>
+                    <div className='btn'>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                    <button>Archive</button>
+                    </div>
                     </div>
                 </div>
             )
