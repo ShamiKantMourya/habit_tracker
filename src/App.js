748eeacc13990e370b57e2b43ea41faa52f61d59
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-28 22:30:18 +0530

    scss file

--- src/App.js
+++ src/App.js
@@ -1,48 +1,14 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { fakeFetch } from "./fakeFetch";
+import { Routes, Route } from "react-router-dom";
 
-import Homepage from './homepage';
-import AddHabit from './addHabit';
-import './App.css';
+import Homepage from "./Pages/Homepage";
+import "./SCSS/App.scss";
 
 function App() {
-  const [habits, setHabits] = useState([]);
-  const getData = async () => {
-    try {
-      const { status, data } = await fakeFetch("https://example.com/api/habits");
-      if (status === 200) {
-        setHabits(data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const deleteHabit = (obj) => {
-    const newHabitData = habits?.map((habit) => {
-      if(habit.id === obj.id) {
-        
-      }
-    });
-    // setHabits(newHabitData);
-  };
-  const updatetask = (obj) => {
-
-  };
-  useEffect(() => {
-    getData();
-  }, []);
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path='/' element={
-          <Homepage habits = {habits}  deleteHabit = {deleteHabit} updatetask = {updatetask} />
-          }></Route>
-          <Route path='/addHabit' element={<AddHabit />}></Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
     </div>
   );
 }
