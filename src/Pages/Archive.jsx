9326dfd075c47ce7d0fe145c67e13b773ebd8dd9
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-31 22:51:17 +0530

    archive

--- src/Pages/Archive.jsx
+++ src/Pages/Archive.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import Habit from '../Components/Habit';
 
 const Archive = () => {
+  const {Archive} = useSelector(state => state.habits);
+  // console.log(Archive, "Archive");
   return (
-    <div>Archive</div>
+    <div className='archive'>
+    {
+      Archive?.map((habit) => 
+      <Habit key={habit.id} habit = {habit} />
+      )
+    }     
+    </div>
   )
 }
 
