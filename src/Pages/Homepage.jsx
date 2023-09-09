46df48bfca695123aef7edcd31558e18a59e9155
Shami_Mourya <shamimourya9852@gmail.com>
2023-09-09 10:54:02 +0530

    homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 
 import AddHabitModel from "../Components/AddHabitModel";
 import "../SCSS/Homepage.scss";
 import Header from "../Components/Header";
 import Habit from "../Components/Habit";
+import ArchivePage from "../Components/ArchivePage";
 
 const Homepage = () => {
   const { habitModel, Habits } = useSelector((state) => state.habits);
+  const dispatch = useDispatch();
   return (
     <div className="homepage">
       <div className="homepage-content">
         <Header />
+        <ArchivePage />
         {habitModel ? (
           <AddHabitModel />
         ) : (
           <div className="habit-data">
-            <div className="habit-box">
+            <div className="habit-box" onClick={()=> dispatch({
+              type: "SET_HABITS",
+              payload: true
+            })}>
               <div className="img-box">
                 <img
                   src="https://img.freepik.com/free-vector/boost-your-immune-system-concept-illustration_114360-2833.jpg?w=740&t=st=1693396863~exp=1693397463~hmac=cdbce8e63d0ec3a98940fc23deab2c28fbd0528725c48c34f5ea0815999aa4ee"
