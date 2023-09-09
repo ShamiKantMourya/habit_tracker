01f8c43d446556816cd21995b6cf483aa47cfc6c
Shami_Mourya <shamimourya9852@gmail.com>
2023-09-09 15:08:14 +0530

    image change

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -25,7 +25,7 @@ const Homepage = () => {
             })}>
               <div className="img-box">
                 <img
-                  src="https://img.freepik.com/free-vector/boost-your-immune-system-concept-illustration_114360-2833.jpg?w=740&t=st=1693396863~exp=1693397463~hmac=cdbce8e63d0ec3a98940fc23deab2c28fbd0528725c48c34f5ea0815999aa4ee"
+                  src="https://img.freepik.com/free-vector/appointment-booking-with-calendar_23-2148549798.jpg?w=740&t=st=1694252222~exp=1694252822~hmac=8351ae19282aae437243af3e7fbec25f87fd06500e9e721d7613eb358ba0949c"
                   alt="Add Habit"
                 />
               </div>
