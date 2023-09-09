d80b7de12f72b5b68a42a062b13da68fdb99e8a6
Shami_Mourya <shamimourya9852@gmail.com>
2023-09-09 10:52:22 +0530

    archive route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Homepage from "./Pages/Homepage";
 import "./SCSS/App.scss";
+import Archive from "./Pages/Archive";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/archive" element={<Archive />} />
       </Routes>
     </div>
   );
