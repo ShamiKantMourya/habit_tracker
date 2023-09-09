a9a21a68d117c8424b3f767386707775c323c626
Shami_Mourya <shamimourya9852@gmail.com>
2023-09-09 10:52:36 +0530

    archive page route

---
+++ src/Components/ArchivePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const ArchivePage = () => {
+  return (
+    <div className='archive-route'>
+    <Link to="/archive">Archive</Link>
+    </div>
+  )
+}
+
+export default ArchivePage;
