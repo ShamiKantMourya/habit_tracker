3cd5780a30de9257f2677e6b89dc6129034511bf
Shami_Mourya <shamimourya9852@gmail.com>
2023-09-09 10:53:38 +0530

    archive page

--- src/Pages/Archive.jsx
+++ src/Pages/Archive.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import {useSelector, useDispatch} from "react-redux";
-import Habit from '../Components/Habit';
+import React from "react";
+import { useSelector } from "react-redux";
+import Habit from "../Components/Habit";
+import Header from "../Components/Header";
+import { useNavigate } from "react-router-dom";
+
+import "../SCSS/Archive.scss";
 
 const Archive = () => {
-  const {Archive} = useSelector(state => state.habits);
+  const { Archive } = useSelector((state) => state.habits);
   // console.log(Archive, "Archive");
+  const navigate = useNavigate();
   return (
-    <div className='archive'>
-    {
-      Archive?.map((habit) => 
-      <Habit key={habit.id} habit = {habit} />
-      )
-    }     
+    <div className="archive">
+      <div className="archive-content">
+        <Header />
+        <div className="navigate-btn">
+          <button className="back-btn" onClick={() => navigate(-1)}>
+            Back
+          </button>
+        </div>
+        <div className="habit-data">
+          {Archive?.map((habit) => (
+            <Habit key={habit.id} habit={habit} />
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Archive;
